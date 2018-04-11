617c0d6861c9d4a42c8e516cb1476152d1e2cebc
kjkwer <lilinfeng1024@163.com>
2018-04-11 18:23:17 +0800

    数量统计 发送数量和接收数量合并到一个表上

--- application/controllers/show/StatisticsController.class.php
+++ application/controllers/show/StatisticsController.class.php
@@ -30,53 +30,43 @@ class StatisticsController extends BaseController
         include CUR_VIEW_PATH."Sstatistics" . DS . "statistics_send.html";
     }
 
-    public function acceptAction(){
-
-        include CUR_VIEW_PATH."Sstatistics" . DS . "statistics_accept.html";
-    }
-
     public function sendDataAction(){
         $dayNum = $_POST["dayNum"];
         $n = array_search($dayNum,self::$dayNum);
         $days = array();
         $num = array();
+        $sendNum = array();
+        $acceptNum = array();
         for ($i=0;$i<$n;$i++){
             $days[] = date("Y-m-d",time()-24*3600*$i);
         }
         $days = array_reverse($days);
+        $model = new ModelNew("gw");
         if ($_POST["type"]==1){
             if(self::$userData["guanliyuan"]==1){
-                $model = new ModelNew("gw");
                 foreach ($days as $v){
                     $count = $model->findBySql("select count(*) as total from sl_gw where Date(dtime) BETWEEN '$v' AND '$v'");
-                    $num[] = $count[0]['total'];
+                    $sendNum[] = $count[0]['total'];
                 }
+                foreach ($days as $v){
+                    $count = $model->findBySql("select count(*) as total from sl_gw_o where Date(sendTime) BETWEEN '$v' AND '$v' AND zhuangtai>1");
+                    $acceptNum[] = $count[0]['total'];
+                }
+
             }else{
-                $model = new ModelNew("gw_o");
                 $zuozhe = self::$userData["id"];
                 foreach ($days as $v){
                     $count = $model->findBySql("select count(*) as total from sl_gw_o where  Date(sendTime) BETWEEN '$v' AND '$v' AND zuozhe=$zuozhe AND zhuangtai>1");
-                    $num[] = $count[0]['total'];
+                    $sendNum[] = $count[0]['total'];
                 }
-            }
-
-        }else{
-            if(self::$userData["guanliyuan"]==1) {
-                $model = new ModelNew("gw_o");
-                foreach ($days as $v){
-                    $count = $model->findBySql("select count(*) as total from sl_gw_o where Date(sendTime) BETWEEN '$v' AND '$v' AND zhuangtai>1");
-                    $num[] = $count[0]['total'];
-                }
-            }else{
-                $model = new ModelNew("fasong");
                 $zuozhe = self::$userData["id"];
                 foreach ($days as $v){
                     $count = $model->findBySql("select count(*) as total from sl_fasong where Date(dtime) BETWEEN '$v' AND '$v' AND jieshouren=$zuozhe");
-                    $num[] = $count[0]['total'];
+                    $acceptNum[] = $count[0]['total'];
                 }
             }
 
         }
-        echo json_encode(["days"=>$days,"nums"=>$num]);
+        echo json_encode(["days"=>$days,"sendNum"=>$sendNum,"acceptNum"=>$acceptNum]);
     }
 }
